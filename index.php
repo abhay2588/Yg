4f3daaa92e8e18d9f5200a42aed173741786c169
abhay2588 <160823533+abhay2588@users.noreply.github.com>
2024-11-06 19:48:46 +0530

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $get = $_GET['get'];
-$mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/' . $get;
+$mpdUrl = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/2504/default.mpd?|drmScheme=clearkey&drmLicense=03c2e0af2f8159f9f0ce9b5dbc865f10:cd84ed136b0cc71f8ab8cd4d4f6a2e4c' . $get;
 
 $mpdheads = [
   'http' => [
